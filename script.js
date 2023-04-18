6937c77d0662b7c1295f0d21c1911707cf2b8037
aixellelethe <125380826+aixellelethe@users.noreply.github.com>
2023-04-18 14:24:02 -0400

    Started adding functions to js file

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //Rock Paper Scissors//
+
+
+//Get a random selection of rock paper scissors from computer using an array//
+//Using math floor to ensure correct integers, and then random based on the array length//
+
+function getComputerChoice(){
+
+    const computerHand = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random()* computerHand.length);
+    return computerHand[randomIndex];
+}
+
+//Log computer selection//
+const computerSelection = getComputerChoice();
+
+
+//Get a choice from the player//
+
+// Format choice, check to see if its valid, then give value//
+
+function getPlayerChoice(){
+
+    let playerHandCheck= prompt("Rock, paper or scissors?");
+        playerHand = playerHandCheck.toLowerCase();
+
+        if ( playerHand != null && playerHand === "rock" || playerHand === "paper" || playerHand ==="scissors"){
+            return playerHand;
+
+
+        }   else {
+            console.log("Invalid");
+            return getPlayerChoice();
+        }
+}
+// Log player selection//
+const playerSelection = getPlayerChoice();
+
+
+function playRound (playerSelection, computerSelection){
+
+
+}
